37b1769a394ae5866a0c2226d06b89ea9067c2d0
Syed Owais Noor <50991352+Syed-Owais-Noor@users.noreply.github.com>
2024-12-10 08:11:34 +0500

    [10 Dec 24] [Finalized the files]

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-    port: 8001,
-  },
-  plugins: [react()],
+        host: '0.0.0.0',  // Allow access from any IP address (including Docker containers)
+        port: 8001,
+    },
+    plugins: [react()],
 })
